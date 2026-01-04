4b87975187d61ded893ff8014796033d79a1e5c8
SimiRiza <simiriza14@gmail.com>
2026-01-04 18:38:00 +0530

    Add setZeroes function for matrix zeroing
    Implement in-place matrix zeroing using first row and column as markers.

--- 01-array-traversal/73_setmatrixzeroes_constant_space.cpp
+++ 01-array-traversal/73_setmatrixzeroes_constant_space.cpp
@@ -1,30 +1,70 @@
 #include <vector>
-#include <iostream>
 using namespace std;
-int main() {
-    int m = 3;
-    int n = 4;
- vector<vector<int>> matrix = {
-        { 1,  2,  0,  6},
-        { 0,  6, 0, 1},
-        {7, 2, 1, 3}
-    };
-        
+// LeetCode: Set Matrix Zeroes
+// Approach: Use 1st row and column instead of row and column arrays ->inplace marking
+// Time Complexity: O(m * n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        // Flags to track whether the first row or first column
+        // originally contained any zero
+        int first_row0 = 0;
+        int first_col0 = 0;
+
+        // First pass:
+        // Use the first row and first column as markers
+        // to indicate which rows and columns should be zeroed
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+
+                    // If zero is found in first row, mark it separately
+                    if (i == 0) first_row0 = 1;
+
+                    // If zero is found in first column, mark it separately
+                    if (j == 0) first_col0 = 1;
+
+                    // For all other cells, mark corresponding row and column
+                    else {
+                        matrix[i][0] = 0;  // mark row
+                        matrix[0][j] = 0;  // mark column
+                    }
+                }
+            }
+        }
+
         // Second pass:
-        // Set matrix elements to zero if their row or column is marked
+        // Zero out cells based on markers in first row and first column
+        // Skip first row and first column for now
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+
+                    // First row and first column are handled separately later
+                    if (i == 0 || j == 0) continue;
+
                     matrix[i][j] = 0;
                 }
             }
         }
-        //print matrix
-        for (int i = 0; i < m; i++) {
+
+        // If first row originally had a zero, zero the entire first row
+        if (first_row0) {
             for (int j = 0; j < n; j++) {
-                cout << matrix[i][j] << " ";
+                matrix[0][j] = 0;
             }
-            cout << endl;
+        }
+
+        // If first column originally had a zero, zero the entire first column
+        if (first_col0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
         }
     }
-
+};
