d8bd8324b742127daca7d7ae93091451d040e0c9
SimiRiza <simiriza14@gmail.com>
2026-03-06 20:33:35 +0530

    fix bug in 0976 2_ptr

--- 04-Sorting-Pattern/0976_largest_perimeter/0976_two_pointer.cpp
+++ 04-Sorting-Pattern/0976_largest_perimeter/0976_two_pointer.cpp
@@ -5,13 +5,14 @@
 using namespace std;
 
 /*
-Approach: Sorted Two-Pointer Exploration (Attempt toward O(n^2))
+Approach: Sorted Two-Pointer Exploration
 
 Description:
-The array is sorted first. For each element treated as the largest side `c`,
-two pointers `a` (left) and `b` (right before c) move inward. If the triangle
-inequality nums[a] + nums[b] > nums[c] holds, we compute a candidate perimeter
-and try to maximize it.
+The array is first sorted. For each element treated as the largest side `c`,
+two pointers `a` (start) and `b` (just before c) move inward.
+
+If nums[a] + nums[b] > nums[c], triangle inequality holds.
+We compute the perimeter and update the maximum.
 
 Time Complexity: O(n^2)
 Space Complexity: O(1)
@@ -24,25 +25,19 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        int curr_m = 0, m = 0;
+        int m = 0;
 
-        for (int c = n - 1; c >= 2; c--) {
+        for(int c = n - 1; c >= 2; c--) {
+            int a = 0, b = c - 1;
 
-            int a = 0;
-            int b = c - 1;
-
-            while (a < b) {
-
-                if (nums[a] + nums[b] > nums[c]) {
-
-                    // candidate triangle
-                    curr_m = nums[a] + nums[b] + nums[c];
-                    m = max(curr_m, m);
-
-                    b--; // explore next possible pair
-
-                } else {
-                    a++; // increase sum
+            while(a < b) {
+                if(nums[a] + nums[b] > nums[c]) {
+                    m = max(m, nums[a] + nums[b] + nums[c]);
+                    b--;
+                    break;
+                }
+                else{
+                    a++;
                 }
             }
         }
@@ -54,11 +49,13 @@ public:
 int main() {
     Solution sol;
 
-    vector<int> test1 = {2, 1, 2};
-    cout << "Test1: " << sol.largestPerimeter(test1) << endl;
+    vector<int> nums1 = {2,1,2};
+    vector<int> nums2 = {1,2,1};
+    vector<int> nums3 = {3,6,2,3};
 
-    vector<int> test2 = {3, 2, 3, 4};
-    cout << "Test2: " << sol.largestPerimeter(test2) << endl;
+    cout << "Test1: " << sol.largestPerimeter(nums1) << endl;
+    cout << "Test2: " << sol.largestPerimeter(nums2) << endl;
+    cout << "Test3: " << sol.largestPerimeter(nums3) << endl;
 
     return 0;
 }
