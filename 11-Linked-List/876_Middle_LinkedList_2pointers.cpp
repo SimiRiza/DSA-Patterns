c10c176069adc69ff606da6511c3dd3dda98e5f0
SimiRiza <simiriza14@gmail.com>
2026-06-20 18:29:41 +0530

    Solve LeetCode 876 Middle of the Linked List

---
+++ 11-Linked-List/876_Middle_LinkedList_2pointers.cpp
@@ -0,0 +1,65 @@
+/*
+Approach:
+Use slow and fast pointers.
+Slow moves one step at a time.
+Fast moves two steps at a time.
+When fast reaches the end, slow will be at the middle.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution obj;
+
+    ListNode* ans = obj.middleNode(head);
+
+    cout << "Middle Node Value: " << ans->val << endl;
+
+    return 0;
+}
+
